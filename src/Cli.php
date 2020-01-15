1375c6e49c714caf9e1d069993f7b3e336b6f41f
Ruslan Knyazev <agrry@yandex.ru>
2020-01-16 01:48:36 +0500

    first implementation brain-even

--- src/Cli.php
+++ src/Cli.php
@@ -8,6 +8,28 @@ use function cli\prompt;
 function run()
 {
     line('Welcome to the Brain Games!');
+    line('Answer "yes" if the number is even, otherwise answer "no"' . PHP_EOL);
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    line("Hello, %s!" . PHP_EOL, $name);
+
+    for ($i = 1; $i <= 3; $i++) {
+        $number = rand(1, 99);
+        $message = "Question: {$number}";
+        $answer = prompt($message);
+        $realAnswer = isEven($number) ? 'yes' : 'no';
+        if ($answer === $realAnswer) {
+            line("Correct!");
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $realAnswer);
+            line("Let's try again, %s!", $name);
+            return;
+        }
+    }
+
+    line('Congratulations, %s!', $name);
+}
+
+function isEven(int $number)
+{
+    return $number % 2 === 0;
 }
