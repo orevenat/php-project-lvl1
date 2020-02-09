74dd8bf1b7940eac1fa3c0205cb1bafecf5870d0
Ruslan Knyazev <agrry@yandex.ru>
2020-02-09 15:36:30 +0500

    finish fixes

--- src/Engine.php
+++ src/Engine.php
@@ -5,14 +5,14 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function run(string $description, callable $runGame, int $roundCount = 3)
+function run(string $description, callable $runGame, int $roundsCount = 3)
 {
     line('Welcome to the Brain Games!');
     line($description);
     $name = prompt('May I have your name?');
-    line("Hello, %s!" . PHP_EOL, $name);
+    line("Hello, %s!", $name);
 
-    for ($i = 1; $i <= $roundCount; $i++) {
+    for ($i = 1; $i <= $roundsCount; $i++) {
         [$question, $correctAnswer] = $runGame();
 
         $questionMessage = "Question: {$question}";
